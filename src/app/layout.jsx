8d98be96e4f05a03a2717cc3c2d21d3d5b89a64c
biribinha0 <bernardomadureira.souza@gmail.com>
2025-10-17 13:54:39 -0300

    first commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar04Page from "@/components/navbar-04/navbar-04";
+import Footer04Page from "@/components/footer-04/footer-04";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import { ModeToggle } from "@/components/theme/theme-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +26,18 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar04Page />
+          {children}
+          <Footer04Page />
+          
+        </ThemeProvider>
+
       </body>
     </html>
   );
