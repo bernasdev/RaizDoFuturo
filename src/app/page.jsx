3f51345c555d2a2afedf53f42ec7c9085f86dd73
biribinha0 <bernardomadureira.souza@gmail.com>
2025-10-17 14:11:31 -0300

    Reapply "first commit"
    This reverts commit 7d27dbbb99afec58a8c625e208102c63dfa0fc38.

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Hero01 from "@/components/hero-01/hero-01";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full flex justify-center">
+      <Hero01/>
+    </div>
+  );
+}
